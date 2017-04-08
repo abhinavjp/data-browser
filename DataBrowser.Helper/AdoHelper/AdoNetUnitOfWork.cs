d19e3397975180e9ef5acabd71719a9f00839807
Abhinav <abhinavpisharody.asia@gmail.com>
2017-04-09 02:04:20 +0530

    Feat: Started Readying the AdoHelper class

---
+++ DataBrowser.Helper/AdoHelper/AdoNetUnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using RepositoryFoundation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBrowser.Helper.AdoHelper
+{
+    public class AdoNetUnitOfWork : IUnitOfWork
+    {
+        private IDbTransaction _transaction;
+        private readonly Action<AdoNetUnitOfWork> _rolledBack;
+        private readonly Action<AdoNetUnitOfWork> _committed;
+        public AdoNetUnitOfWork(IDbTransaction transaction, Action<AdoNetUnitOfWork> rolledBack, Action<AdoNetUnitOfWork> committed)
+        {
+            Transaction = transaction;
+            _transaction = transaction;
+            _rolledBack = rolledBack;
+            _committed = committed;
+        }
+        public IDbTransaction Transaction { get; private set; }
+        public void Dispose()
+        {
+            if (_transaction == null)
+                return;
+            _transaction.Rollback();
+            _transaction.Dispose();
+            _rolledBack(this);
+            _transaction = null;
+        }
+        public int Commit()
+        {
+            if (_transaction == null)
+                throw new InvalidOperationException("May not call save changes twice.");
+            _transaction.Commit();
+            _committed(this);
+            _transaction = null;
+            return 1;
+        }
+
+        public Task<int> CommitAsync()
+        {
+            return new Task<int>(() => {
+                return Commit();
+            });
+        }
+    }
+}
---
+++ DataBrowser.Helper/AdoHelper/IConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBrowser.Helper.AdoHelper
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection Create();
+    }
+}
