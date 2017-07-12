095887793c52515b853d228bddc7f6438c92409c
Abhinav <abhinavpisharody.asia@gmail.com>
2017-07-13 00:04:46 +0530

    Feat: Added DatabaseAccessAdapter which provides base for queries

---
+++ DataBrowser.Helper/AdoHelper/DataAccessAdapter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBrowser.Helper.AdoHelper
+{
+    public class DataAccessAdapter
+    {
+        #region Initialization
+        private readonly string _dataSource;
+        private readonly string _password;
+        private DataAccessAdapter(string dataSource, string password)
+        {
+            _dataSource = dataSource;
+            _password = password;
+        }
+
+        public static DataAccessAdapter Initialize(string dataSource, string password)
+        {
+            return new DataAccessAdapter(dataSource, password);
+        }
+        #endregion
+
+        public bool DoEntity<T>(T entity, IDbCommand dbCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GenerateCommand<T>(IDbCommand dbCommand, T entity, string whereClause = "")
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataBrowser.Helper/AdoHelper/Model/Enums.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBrowser.Helper.AdoHelper.Model
+{
+    public enum CommandExecutionType { Insert, Update, Delete, Select }
+}
